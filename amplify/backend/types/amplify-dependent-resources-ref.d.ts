d85df3cdc5a3c20a5e165f2999b58c347b5479aa
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2024-05-04 16:52:30 -0400

    Revert "more updates"
    This reverts commit ed7774e07c0fecaf28c5fbee4e2817cac356ad7f.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "api6132532b": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "function": {
-    "geminiapitestc66932a4": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
