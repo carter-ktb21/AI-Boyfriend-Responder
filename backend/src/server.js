07f47831a4264121efa6b2c764250e8ffb63760f
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2024-05-08 18:39:05 -0400

    added voice stuff

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const path = require("path")
 const cors = require('cors'); // Import cors
 const { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } = require('@google/generative-ai');
+const ElevenLabs = require("elevenlabs-js");
+
+ElevenLabs.setApiKey(process.env.ELEVENLABS_API_KEY)
 
 const app = express();
 const apiKey = process.env.API_KEY; // Retrieve API key from environment variable
@@ -14,9 +18,10 @@ const model = genAI.getGenerativeModel({ model: 'gemini-pro', safetySettings: [{
 
 // Enable All CORS Requests
 app.use(cors());
+app.use(express.json());
 
-app.get('/generateContent', async (req, res) => {
-    const prompt = req.query.prompt; // Retrieve prompt from query parameters
+app.post('/generateContent', async (req, res) => {
+    const prompt = req.body.prompt; // Retrieve prompt from query parameters
     console.log('Received request to generate content with prompt:', prompt); // Log the prompt
   
     try {
@@ -30,7 +35,22 @@ app.get('/generateContent', async (req, res) => {
       const response = result.response;
       const text = response.text();
       console.log('Generated text:', text); // Log the generated text
-      res.json({ text });
+      
+      const audioFile = "test.mp3";
+
+      const absolutePath = path.join(__dirname, "..", audioFile);
+
+      ElevenLabs.textToSpeech("3gsg3cxXyFLcGIfNbM6C", text, "eleven_monolingual_v1", {
+        stability: 0.95,
+        similarity_boost: 0.75,
+        style: 0.06,
+        use_speaker_boost: true
+      }).then(async (labs_res) => {
+        await labs_res.saveFile(audioFile);
+
+        res.sendFile(absolutePath);
+
+      });
     } catch (error) {
       console.error('Error generating content:', error); // Log any errors
       res.status(500).json({ error: error.message });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,35 +4,57 @@ import axios from 'axios';
 function App() {
   const [generatedText, setGeneratedText] = useState('');
   const [input, setInput] = useState('');
+  const [audioUrl, setAudioUrl] = useState('');
 
   const generateContent = async () => {
     const prompt = "Write a summary about " + input + ", but act as if you were me talking to my girlfriend."
-    + " I will now describe myself a little. I'm pretty silly with a good vocabulary. I'm pretty passionate when talking about things that I like, including theology, history, and logic."
-    + " Also, if the subject matter in your response is NOT theology or a heavy subject matter, add cheesy romantic lines and cringey jokes."
-    + " Be short and succint while also thorough. Only write in paragraph format.";
+      + " I will now describe myself a little. I'm pretty silly with a good vocabulary. I'm pretty passionate when talking about things that I like, including theology, history, and logic."
+      + " Also, if the subject matter in your response is NOT theology or a heavy subject matter, add cheesy romantic lines and cringey jokes."
+      + " Be short and succint, keeping your response under 200 characters, but while also being thorough. Only write in paragraph format.";
     console.log(prompt);
     try {
-      const response = await axios.get('https://ai-boyfriend-questions-api.azurewebsites.net/api/generateContent?code=HBMXVZdkDbwLH56gkAgVvBfEjhWIT8VmRuIKiD_lkxpBAzFuWZEBGw%3D%3D', {
-        params: { prompt },
+      const response = await axios.post('https://ai-boyfriend-questions-api.azurewebsites.net/api/generateContent?code=HBMXVZdkDbwLH56gkAgVvBfEjhWIT8VmRuIKiD_lkxpBAzFuWZEBGw%3D%3D', {
+        prompt: prompt
+      }, {
+        responseType: 'blob',
       });
-      setGeneratedText(response.data.text);
+      getAudioUrl();
     } catch (error) {
       console.error('Error generating content:', error);
     }
   };
 
+  const getAudioUrl = async () => {
+    try {
+      // Make a request to the getAudioFunction Azure Function
+      const response = await axios.get('https://ai-boyfriend-questions-api.azurewebsites.net/api/getAudioFile?code=b4yvN1ihW2mu0TmWkVDD759onuXxIhyr5wVCyrjdN9LiAzFufOjxKA==');
+      // Extract the SAS URL from the response
+      const sasUrl = response.data.sasUrl;
+      // Set the SAS URL in state
+      setAudioUrl(sasUrl);
+    } catch (error) {
+      console.error('Error getting audio URL:', error);
+    }
+  };
+
   return (
-    <div style={{display:"flex", flexDirection:"column", alignItems:"center", padding:"100px"}}>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "100px" }}>
       <h1>What did your boyfriend talk about this time?</h1>
       <input
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Enter prompt"
-        style={{margin:"20px"}}
+        style={{ margin: "20px" }}
       />
       <button onClick={generateContent}>Generate</button>
       <p>{generatedText}</p>
+      {audioUrl && (
+        <audio controls>
+          <source src={audioUrl} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      )}
     </div>
   );
 }
